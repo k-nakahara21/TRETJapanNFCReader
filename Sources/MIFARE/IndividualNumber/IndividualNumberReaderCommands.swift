3205ee7226a7772a50111556a18bf0cd3340dcfe
Kenta Nakahara <k-nakahara@abelsoft.co.jp>
2023-04-04 14:19:23 +0900

    署名用コマンド関数（computeDigitalSignature）を追加

--- Sources/MIFARE/IndividualNumber/IndividualNumberReaderCommands.swift
+++ Sources/MIFARE/IndividualNumber/IndividualNumberReaderCommands.swift
@@ -38,6 +38,12 @@ extension IndividualNumberReader {
         tag.sendCommand(apdu: adpu, completionHandler: completionHandler)
     }
     
+    internal func computeDigitalSignature(tag: IndividualNumberCardTag, data: [UInt8],completionHandler: @escaping IndividualNumberReaderCompletionHandler) {
+        let apdu = NFCISO7816APDU(instructionClass: 0x80, instructionCode: 0x2A, p1Parameter: 0x00, p2Parameter: 0x80, data: Data(dataToSign), expectedResponseLength: 0x00)
+        
+        tag.sendCommand(apdu: adpu, completionHandler: completionHandler)
+    }
+    
     internal func selectJPKIAP(tag: IndividualNumberCardTag, completionHandler: @escaping IndividualNumberReaderCompletionHandler) {
         let data = IndividualNumberCardAID.jpkiAP
         self.selectDF(tag: tag, data: data, completionHandler: completionHandler)
